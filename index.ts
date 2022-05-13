b8893cfecd575b71b65eae54dbc184c02b2c52b7
Simon <salviensky@gmail.com>
2022-05-13 09:36:17 +0100

    Fixed that annoying ESM conflicts

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import { config } from "dotenv"
-import { Exchange } from "ccxt"
+import Exchange from "ccxt"
 
 config() // Take environment variables from .env
 
-const ftx = new Exchange({
-    key : 'ftx',
+const ftx = new Exchange.ftx({
     'apiKey' : process.env.API_KEY,
-    'secret' : process.env.API_SECRET
+    'secret' : process.env.API_SECRET,
+
 })
+
+let portfolio = await ftx.fetchBalance()
+
+console.log(JSON.stringify(portfolio, undefined, 4));
